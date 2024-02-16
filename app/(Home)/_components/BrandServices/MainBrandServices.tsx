876df56a03759e8b376ebe76a1c234bf038b9d2a
david temitope <temi236topemicheal@gmail.com>
2024-02-16 16:33:12 +0100

    Feature(HomeSectionDesign):work on some sections in home section

--- app/(Home)/_components/BrandServices/MainBrandServices.tsx
+++ app/(Home)/_components/BrandServices/MainBrandServices.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const MainBrandServices = (props: Props) => {
   return (
     <>
-      <div className="h-screen flex flex-col lg:flex-row gap-y-8 lg:divide-x-[1px] divide-black ContainerPadding py-8">
+      <div className="flex flex-col lg:flex-row gap-y-8 lg:divide-x-[1px] divide-black ContainerPadding py-8">
         <Connect />
         <Experience />
       </div>
