f3b50605fe442152a611a87f34a86f05d21887d4
david temitope <temi236topemicheal@gmail.com>
2024-02-16 19:06:24 +0100

    Feature(HomeSectionDesign):work on some sections in home section

--- app/(Home)/_components/Team/SubComponents/TeamDescription.tsx
+++ app/(Home)/_components/Team/SubComponents/TeamDescription.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const TeamDescription = (props: Props) => {
   return (
     <div className="py-8  lg:pr-8 w-full h-[75vh] lg:h-[145.9vh]">
-      <div className="h-full bg-red-600">hi</div>
+      <div className="h-full bg-red-600">team description</div>
     </div>
   );
 };
