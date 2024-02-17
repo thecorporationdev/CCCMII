d7e2db49f1dc3fcee49e9b908d9946cb1991cb09
david temitope <temi236topemicheal@gmail.com>
2024-02-17 10:43:45 +0100

    Feature(Fullpage Skeleton):Work on Page structure

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <header className="flex gap-x-4 py-4 w-full items-center ContainerPadding  container">
+    <header className="flex gap-x-4 py-4 w-full items-center ContainerPadding container max-lg:px-4">
       <Logo />
       <NavMenu />
     </header>
