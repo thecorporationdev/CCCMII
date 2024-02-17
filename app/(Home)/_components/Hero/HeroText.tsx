91b568036fe96b89aa0f5833c97baa8028a5a4fe
david temitope <temi236topemicheal@gmail.com>
2024-02-17 15:00:19 +0100

    Refcator(Herogrids):creating new grids for hero section

--- app/(Home)/_components/Hero/HeroText.tsx
+++ app/(Home)/_components/Hero/HeroText.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroText = () => {
   return (
     <div className="text-3xl sm:text-5xl font-medium lg:px-8 mb-16 lg:mb-24 max-lg:px-6 tracking-tighter">
-      WELCOME TO THE OFFICIAL,WEBSITE OF THREE C&apos;s MET
+      WELCOME TO THE OFFICIAL WEBSITE OF THREE C&apos;s MET
       <br className="max-lg:hidden" /> INTERNATIONAL INSTITUTE OF MEDIATION,
       <br className="max-lg:hidden" /> NEGOTIATION, COUNSELING AND
       CONCILIATION(CCCMII)
