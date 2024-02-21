34c035703d28385a2ee090199d592d581f658199
david temitope <temi236topemicheal@gmail.com>
2024-02-21 12:26:35 +0100

    refactor:Change header texts and hero text alignment

--- app/(Home)/_components/About/About.tsx
+++ app/(Home)/_components/About/About.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const About = (props: Props) => {
   return (
     <div className="lg:px-10  py-20 lg:py-40 text-3xl md:text-5xl  uppercase flex lg:justify-end font-medium tracking-tight ContainerPadding">
-      <div className="lg:w-[60%] ">
+      <div className="lg:w-[65%] ">
         At CCCMII, we are dedicated to empowering individuals and organizations
         with the skills and knowledge necessary for effective mediation,
         negotiation, counselling, and conciliation
