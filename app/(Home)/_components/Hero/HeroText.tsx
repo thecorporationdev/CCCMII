e743ff24a1e59c3e03f9d92e01e6d8e8c2750a67
david temitope <temi236topemicheal@gmail.com>
2024-02-21 12:20:58 +0100

    refactor:Change header texts and hero text alignment

--- app/(Home)/_components/Hero/HeroText.tsx
+++ app/(Home)/_components/Hero/HeroText.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeroText = () => {
   return (
     <div className="text-3xl sm:text-5xl font-medium lg:px-8 mb-16 lg:mb-24 max-lg:px-6 tracking-tight flex justify-end">
-      <div className="w-full lg:w-1/2">
+      <div className="w-full lg:w-[60%]">
         WELCOME TO THE OFFICIAL WEBSITE OF THREE C&apos;s MET
         <br className="max-lg:hidden" /> INTERNATIONAL INSTITUTE OF
         MEDIATION,NEGOTIATION,
