b5d4bb07c2fe786ebb61dde50e275935260f110f
david temitope <temi236topemicheal@gmail.com>
2024-02-21 12:06:08 +0100

    refactor:Change header texts and hero text alignment

--- app/(Home)/_components/Hero/HeroText.tsx
+++ app/(Home)/_components/Hero/HeroText.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const HeroText = () => {
   return (
-    <div className="text-3xl sm:text-5xl font-medium lg:px-8 mb-16 lg:mb-24 max-lg:px-6 tracking-tight">
-      WELCOME TO THE OFFICIAL WEBSITE OF THREE C&apos;s MET
-      <br className="max-lg:hidden" /> INTERNATIONAL INSTITUTE OF
-      MEDIATION,NEGOTIATION,
-      <br className="max-lg:hidden" /> COUNSELING AND CONCILIATION (CCCMII)
+    <div className="text-3xl sm:text-5xl font-medium lg:px-8 mb-16 lg:mb-24 max-lg:px-6 tracking-tight flex justify-end">
+      <div className="w-full lg:w-1/2">
+        WELCOME TO THE OFFICIAL WEBSITE OF THREE C&apos;s MET
+        <br className="max-lg:hidden" /> INTERNATIONAL INSTITUTE OF
+        MEDIATION,NEGOTIATION,
+        <br className="max-lg:hidden" /> COUNSELING AND CONCILIATION (CCCMII)
+      </div>
     </div>
   );
 };
--- components/Navbar/NavMenu.tsx
+++ components/Navbar/NavMenu.tsx
@@ -15,7 +15,7 @@ const NavMenu = (props: Props) => {
   return (
     <div className="border-[1px] border-black flex  justify-end max-lg:px-4 lg:justify-between w-full flex-1 items-center font-semibold text-base uppercase">
       <div className="w-full lg:flex items-center justify-center py-6 hidden cursor-pointer">
-        courses
+        About
       </div>
       <div
         className="border-r-[1px] border-l-[1px] border-black w-full lg:flex items-center cursor-pointer justify-center py-6 hidden "
@@ -23,7 +23,7 @@ const NavMenu = (props: Props) => {
           router.push("#footer");
         }}
       >
-        contact us
+        courses
       </div>
       <div className="w-full items-center justify-end hidden lg:flex py-6 ">
         <div className="flex mr-4 gap-x-3 items-center cursor-pointer">
