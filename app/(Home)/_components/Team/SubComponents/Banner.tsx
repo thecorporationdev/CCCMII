f698039278291cd9ac5433fde97ab71355afeb46
david temitope <temi236topemicheal@gmail.com>
2024-02-22 04:28:19 +0100

    Feat:update on courses page and home page

--- app/(Home)/_components/Team/SubComponents/Banner.tsx
+++ app/(Home)/_components/Team/SubComponents/Banner.tsx
@@ -5,17 +5,17 @@ type Props = {};
 
 const Banner = (props: Props) => {
   return (
-    <div className=" border-b-[1px] border-b-black">
-      <div className="flex items-center lg:divide-x-[1px] lg:divide-black">
-        <div className="w-56">
-          <div className=" text-base lg:text-2xl font-medium w-full uppercase px-4">
+    <div className=" border-b-[1px] border-b-black ">
+      <div className="flex items-center divide-x-[1px] divide-black container">
+        <div className="w-[25%] max-lg:mr-2 lg:w-[20%]">
+          <div className=" text-sm lg:text-2xl font-medium w-full uppercase  lg:px-4">
             cccmii news
           </div>
         </div>
 
-        <div className="flex-1  ">
-          <div className="flex  max-lg:py-6 font-medium text-xl gap-x-4 w-full py-8 container">
-            <Marquee className="space-x-4 container">
+        <div className="flex-1 overflow-hidden">
+          <div className="flex  max-lg:py-6 font-medium overflow-hidden text-sm lg:text-xl gap-x-4 w-full py-8 ">
+            <Marquee className="space-x-4 w-full">
               <a
                 href="https://www.trustmediation.org.uk/mediations-set-to-increase-as-civil-court-backlogs-hit-an-all-time-high/
 "
