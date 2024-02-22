40ab2aee9cfbfb3e9ed8f4f335c9c106d9b20664
david temitope <temi236topemicheal@gmail.com>
2024-02-22 03:41:56 +0100

    Feat:update on courses page and home page

--- app/(Home)/_components/Team/SubComponents/Banner.tsx
+++ app/(Home)/_components/Team/SubComponents/Banner.tsx
@@ -7,14 +7,14 @@ const Banner = (props: Props) => {
   return (
     <div className=" border-b-[1px] border-b-black">
       <div className="flex items-center lg:divide-x-[1px] lg:divide-black">
-        <div className="hidden lg:block lg:w-[20%] ">
-          <div className="text-2xl font-medium w-full uppercase">
+        <div className="">
+          <div className=" text-base lg:text-2xl font-medium w-full uppercase px-4">
             cccmii news
           </div>
         </div>
 
         <div className="flex-1  ">
-          <div className="flex  max-lg:py-6 font-medium text-xl gap-x-4 w-full py-8">
+          <div className="flex  max-lg:py-6 font-medium text-xl gap-x-4 w-full py-8 container">
             <Marquee className="space-x-4">
               <a
                 href="https://www.trustmediation.org.uk/mediations-set-to-increase-as-civil-court-backlogs-hit-an-all-time-high/
